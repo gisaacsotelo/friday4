1a680c8670ef55b9f214b51cd056de9a7502e626
gisaacsotelo <gisaac.sotelo@gmail.com>
2022-06-16 20:16:21 +1200

    pasted template of taco form

--- client/components/Form.jsx
+++ client/components/Form.jsx
@@ -4,7 +4,30 @@ import React from 'react'
 function Form() {
   return (
     <>
-      
+      {/* <form>
+        <label for="meat">Add some meat:</label>
+        <select id="meat" name="meat">
+        {{#each data.meats}}
+        <option value="{{meat_id}}">{{meat_name}}</option>  
+        {{/each}}
+        </select>
+        <br>
+        <label for="veg">Add some veggies:</label>
+        <select id="veg" name="veg">
+        {{#each data.veggies}}
+        <option value="{{veg_id}}">{{veg_name}}</option>
+        {{/each}}
+        </select>
+        <br>
+        <label for="sauce">Which sauce:</label>
+        <select id="sauce" name="sauce">
+        {{#each data.sauces}}
+        <option value="{{sauce_id}}">{{sauce_name}}</option>
+        {{/each}}
+        </select>
+        <div></div>
+        <button>Make my taco</button>
+      </form> */}
     </>
   )
 }
