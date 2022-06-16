7f103c26ee9d57193b9222b204ee6951d809922c
gisaacsotelo <gisaac.sotelo@gmail.com>
2022-06-16 20:02:03 +1200

    add comments

--- client/index.js
+++ client/index.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+// Import Router module from react
 import { BrowserRouter as Router } from 'react-router-dom'
 
 import App from './components/App'
-
+// Add router tag around whole app
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(
     <Router>
