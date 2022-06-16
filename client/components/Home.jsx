20c971ce39361d0741fffd890feba36dc458cafc
gisaacsotelo <gisaac.sotelo@gmail.com>
2022-06-16 20:16:46 +1200

    Basic home page build to test

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -1,12 +1,20 @@
 // Import React and useState hook from react
 import React, { useState } from 'react'
+// IMPORT COMPONENTS
+import Header from './Header'
+import Footer from './Footer'
 
 function Home() {
   // const [count, setCount] = useState(0)
 
   return (
-    <>
-      
+    <> 
+      <Header />
+      <h2>Dont be shy, build a clown</h2>
+      <p>
+        Here is Home Homs
+      </p>
+      <Footer />
     </>
   )
 }
