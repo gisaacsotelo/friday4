02ce3ae557fdf282052dfdb7f6676119e6d6567d
gisaacsotelo <gisaac.sotelo@gmail.com>
2022-06-16 20:06:45 +1200

    header done

--- client/components/Header.jsx
+++ client/components/Header.jsx
@@ -3,9 +3,12 @@ import React from 'react'
 
 function Header() {
   return (
-    <>
-      
-    </>
+    <header>
+      <h1>Dress the clown Reloaded</h1>
+      <p>
+        Harder, Better, Faster, Stronger 
+      </p>
+    </header>
   )
 }
 
