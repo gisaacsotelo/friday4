83502303f5320a575eddb7f77f541085cde98e9f
gisaacsotelo <gisaac.sotelo@gmail.com>
2022-06-16 18:52:43 +1200

    Started todo file

---
+++ todo.js
@@ -0,0 +1,59 @@
+/*
+todo: This file is used just as a todo list.
+(Recomend "Colorful comments" extention for VS for better view of comments)
+
+^ ------------------------------
+^ -- Separation of Concerns:
+^ ------------------------------
+
+
+^ --------------------------------------------------------------------------------------------
+^ -- MVP:
+^ --------------------------------------------------------------------------------------------
+& 0. PRESENTATION:
+
+    & 0.1 - WireFrames
+    & 0.2 - Kanvan Board
+    & 0.3 - Stand ups (schedule)
+    
+& Set up enviroment: Install dependencies
+
+- Define modules that we will use
+$     npm install
+
+& 1. SETUP our Routes, React structure (Components):
+? Note: '*' marks each component on bright green
+
+*  - Header
+    - h1> w title
+    - p> with a slogan/phrase
+
+*  - Footer
+    - p> team members all rights not reserved or something like that
+
+*  - Form -> get name, mood and background
+    todo: find background images (ideas: space, desert, each memeber add a significant one?)
+
+*   - Home -> redirect '/' to '/Home'
+      - shows the initial form 
+        - form: gets a name a mood (music) and a background
+
+*   - Outfit (Where we dress the clown)
+      - clown outfit selection
+
+*   - Clown (Finall clown)
+      - shows our clown name
+      - shows outfit choosen
+      - plays music (mood)
+      - displays background
+
+*   - Edit
+
+*   - Muahaha - Save button directs here, since we dont save stuff atm (Probs stretch)
+
+
+^---------------------------------------------------------------------------------------------
+^ Stretch ideas:
+
+  - Add route to redirect any route that is 404 to Muahaha
+*/
